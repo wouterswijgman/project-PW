22cbe9f4aa62dbe4375ac0b2aa8402b5686d3b64
Wouter Swijgman <wouterswijgman@gmail.com>
2020-05-04 14:02:43 +0200

    new welcoming script

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello Wouter and Pim!")
+print("This will test your programming skills!")
+print("Several data sets are obtained from OpenML, up to you to find an interesting set and to do "
+      "some appropriate analyses. Best of luck!")
