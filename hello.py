04a414a9a6fd618f0034a93aa607d0290c740731
Wouter Swijgman <wouterswijgman@gmail.com>
2020-09-15 13:40:32 +0200

    testing github desktop
    add line to test github desktop in hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@ print("Hello Wouter and Pim!")
 print("This will test your programming skills!")
 print("Several data sets are obtained from OpenML, up to you to find an interesting set and to do "
       "some appropriate analyses. Best of luck!")
+print("This is a new test line for Github Desktop")
